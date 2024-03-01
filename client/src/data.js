b4c3376653e7bc737eeb2249454d1d45efe2292f
abhinya0501 <singhrajputabhishek1234@gmail.com>
2024-03-01 22:57:05 +0530

    commit

---
+++ client/src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+      title: "Music Cart",
+      subtitle: "MERN Stack",
+      description:
+        "Created an E-Commerce Music Cart to view products based on filters, Add to cart option.Implemented user account creation and login features, as well as acomprehensive product filtering and search functionality based onname, color, brand, and price. Developed a product display with Grid view and List view,designed a detailed product page, and implemented a shoppingcart feature for allowing users to add multiple products. ",
+      image: "./musicCart.png",
+      link: "https://cuvette-frontend.vercel.app/",
+    },
+    {
+      title: "Pocket Notes",
+      subtitle: "MERN Stack",
+      description:
+        " Developed Pocket Notes, a React-based note-taking app empowering users to effortlessly create, organize, and customize notes.  Implemented features such as creating separate note groups, customizing with vibrant colors, and utilizing local storage for data persistence.  Implemented connection of different components, freeze screenson pop-ups, toggle between groups, use input textboxes, and implement dynamic headings and images using the map() function.",
+      image: "./pocketNotes.png",
+      link: "https://pocket-notes-brown.vercel.app/",
+    },
+    {
+      title: "Super App",
+      subtitle: "React",
+      description:" Developed the Super App, an all-in-one React platform with features for entertainment, productivity, and convenience. Created meaningful components, navigate between components/pages, validate users, handle data passing, and work with states. Implemented features like fetching and posting data from/to APIs,  saving data using local storage, creating note-taking , stopwatch components and display whether using API.",
+      image: "./superApp.png",
+      link: "https://superapp-gamma.vercel.app/",
+    },
+    {
+      title: "Stone Paper Scissor",
+      subtitle: "HTML CSS Javascript",
+      description:
+        "Game Rules:Each player simultaneously selects one of three shapes: rock, paper, or scissors. Rock defeats scissors, scissors defeat paper, and paper defeats rock. If both players choose the same shape, it’s a draw Objective: The first player to win 5 rounds emerges as the winner. Implementation  Developed using HTML, CSS, and JavaScript. Random module used for computer’s choices. Images displayed for different hand shapes.",
+      image: "./stonePaper.png",
+      link: "https://rock-paper-scissor-beige.vercel.app/",
+    },
+  ];
+
+
+  export const skills=["React","NodeJs","MongoDB","ExpressJS","HTML","CSS","JavaScript","C/C++","DSA","Git HUb"];
+  export const testimonials=[{
+    quote:"Abhishek, our Full Stack Developer, is a true tech wizard! With a demonstrated history of building scalable web applications across diverse industries, Abhishek consistently delivers top-notch solutions. Their ability to optimize code, enhance app performance, and implement robust security practices is truly commendable. We’ve seen a 40% increase in app performance thanks to Abhishek’s expertise. Plus, their focus on security has reduced system vulnerabilities by an impressive 70%. If you’re looking for a versatile developer who can handle both front-end and back-end challenges, Abhishek is your go-to person!",
+    image:"./aspireLogo.png",
+    name:"Abhishek Rathor",
+    company:"AspireVision Tech",
+  }];
